a81f4f7222be46bc999ad7b672288a23ff6535bc
kkai <kkai@yahoo-corp.jp>
2019-12-16 18:22:16 +0900

    add comment for localApi

--- screwdriver/screwdriver_local.go
+++ screwdriver/screwdriver_local.go
@@ -1,11 +1,13 @@
 package screwdriver
 
+// localApi is a Dummy Screwdriver API endpoint for local mode.
 type localApi struct{}
 
 func NewLocal() (API, error) {
 	return API(localApi{}), nil
 }
 
+// Don't update step lines in local-mode
 func (a localApi) UpdateStepLines(stepName string, lineCount int) error {
 	return nil
 }
