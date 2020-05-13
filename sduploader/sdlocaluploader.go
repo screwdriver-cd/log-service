4f10f9957dad9a24eb7f6654b9a9b8d6afc5c729
kkawahar <xi.reich8@gmail.com>
2020-05-13 10:43:53 +0900

    fix: duplicate logs (#32)

--- sduploader/sdlocaluploader.go
+++ sduploader/sdlocaluploader.go
@@ -1,6 +1,8 @@
 package sduploader
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"os"
 )
@@ -13,6 +15,25 @@ func NewLocalUploader(logFile string) SDUploader {
 	return &sdLocalUploader{
 		logFile: logFile,
 	}
+}
+
+func getLastLine(filePath string) (string, error) {
+	lastLine := ""
+	p, err := os.Open(filePath)
+	if err != nil {
+		return lastLine, err
+	}
+	defer p.Close()
+
+	scanner := bufio.NewScanner(p)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 0 {
+			lastLine = line
+		}
+	}
+
+	return lastLine, nil
 }
 
 func (s *sdLocalUploader) Upload(path string, filePath string) error {
@@ -28,9 +49,33 @@ func (s *sdLocalUploader) Upload(path string, filePath string) error {
 	}
 	defer output.Close()
 
-	_, err = io.Copy(output, input)
+	// Skip lines that have already been logged
+	lastLine, err := getLastLine(s.logFile)
 	if err != nil {
 		return err
+	}
+	inputScanner := bufio.NewScanner(input)
+	matched := false
+	if len(lastLine) > 0 {
+		for inputScanner.Scan() {
+			if matched {
+				_, err = output.Write(([]byte)(fmt.Sprintf("%s\n", inputScanner.Text())))
+				if err != nil {
+					return err
+				}
+			} else if lastLine == inputScanner.Text() {
+				matched = true
+			}
+		}
+	}
+
+	// Output all if there are no lines already logged
+	if !matched {
+		input.Seek(0, 0)
+		_, err = io.Copy(output, input)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
